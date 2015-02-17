c35192225e8852f6f715936c8595ff47a9a48e42
Muhammad Riza <muhammadriza@gmail.com>
2015-02-17 19:30:40 +0700

    comments added

--- session.php
+++ session.php
@@ -1,5 +1,8 @@
 <?php
+// ## mulai sesi
 session_start ();
+
+// ## alihkan ke halaman login jika sesi yang sah tidak ada
 if (! isset ( $_SESSION ['user_id'] )) {
 	header ( 'Location: ' . $home_uri . 'index.php' );
 }
